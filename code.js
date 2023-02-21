a322c66885e315ef3a70245d5e373c5ae9f85d64
IsakNyberg21F <isak.nyberg@elev.ga.ntig.se>
2023-02-21 16:31:49 +0100

    fixade användarvänligheten för shopping cart

--- code.js
+++ code.js
@@ -18,30 +18,52 @@ document.getElementById("popup-2").classList.toggle("active");
 	}
 });
 
+let isInCart1 = false
+let isInCart2 = false
+let isInCart3 = false
 
 
 function addToList(product){
-    const container = document.getElementById('listContent');
-
-    const line = document.createElement('div');
-    line.id = "line";
-
-    line.style.borderTop = "1px solid black";
-
-    container.appendChild(line)
-
-    // Create a new div element
-    const newDiv = document.createElement('div');
-
-    // Set some properties of the new element
-    newDiv.id = product;
-
-    // Add the new element to the document
-    container.appendChild(newDiv);
-
     fetch('data.json')
       .then(response => response.json())
       .then(data => {
+        if(product === data[0].product1.id && quantity1 === 0){
+            return;
+        }
+        else if(product === data[0].product2.id && quantity2 === 0){
+            return;
+        }
+        else if(product === data[0].product3.id && quantity3 === 0){
+            return;
+        }
+
+        if(product === data[0].product1.id && isInCart1 === true){
+            return;
+        }
+        else if(product === data[0].product2.id && isInCart2 === true){
+            return;
+        }
+        else if(product === data[0].product3.id && isInCart3 === true){
+            return;
+        }
+
+        const container = document.getElementById('listContent');
+
+        const line = document.createElement('div');
+        line.id = "line";
+    
+        line.style.borderBottom = "2px solid black";
+    
+        // Create a new div element
+        const newDiv = document.createElement('div');
+    
+        // Set some properties of the new element
+        newDiv.id = product;
+    
+        // Add the new element to the document
+        container.appendChild(newDiv);   
+        container.appendChild(line) 
+
         // Create an img element
         const imgElement = document.createElement("img");
         const textElement = document.createElement("p");
@@ -88,16 +110,20 @@ function addToList(product){
         closeButton.id = "closeButton";
 
         // add an event listener to the button to handle the click event
+        
         closeButton.addEventListener('click', function() {
             // code to close or hide something goes here
             if(product === data[0].product1.id){
                 document.getElementById(data[0].product1.id).remove();
+                isInCart1 = false;
             }
             else if(product === data[0].product2.id){
                 document.getElementById(data[0].product2.id).remove();
+                isInCart2 = false;
             }
             else if(product === data[0].product3.id){
                 document.getElementById(data[0].product3.id).remove();
+                isInCart3 = false;
             }
             document.getElementById("line").remove();
         });
@@ -107,21 +133,29 @@ function addToList(product){
         newDiv.appendChild(textElement);
 
         // create a new span element
-        const spanElement = document.createElement('span');
-
+        var spanElement = document.createElement('span');
+        console.log(isInCart3);
         // set the id attribute of the span element
-        if(product === data[0].product1.id){
+        if(product === data[0].product1.id && isInCart1 === false){
             spanElement.setAttribute('id', 'quantity1');
+            spanElement.textContent = quantity1;
+            isInCart1 = true;
         }
-        else if(product === data[0].product2.id){
+        else if(product === data[0].product2.id && isInCart2 === false){
             spanElement.setAttribute('id', 'quantity2');
+            spanElement.textContent = quantity2;
+            isInCart2 = true;
         }
-        else if(product === data[0].product3.id){
+        else if(product === data[0].product3.id && isInCart3 === false){
             spanElement.setAttribute('id', 'quantity3');
+            spanElement.textContent = quantity3;
+            isInCart3 = true;
         }
+        console.log(isInCart3);
 
-        // set the text content of the span element to "0"
-        spanElement.textContent = '0';
+        var amountText = document.createElement("p");
+        amountText.innerText = "Amount:";
+        newDiv.appendChild(amountText);
 
         // add the span element to the DOM (e.g., as a child of another element)
         newDiv.appendChild(spanElement);
@@ -134,16 +168,16 @@ let quantity2 = 0;
 let quantity3 = 0;
 
 function addProduct(product) {
-    if(product === "wheels"){
+    if(product === "wheels" && isInCart1 === false){
         quantity1++;
         document.getElementById("quantity1").innerHTML = quantity1;
 
     }
-    else if(product === "cleaningKit"){
+    else if(product === "cleaningKit" && isInCart2 === false){
         quantity2++;
         document.getElementById("quantity2").innerHTML = quantity2;
     }
-    else if(product === "carTools"){
+    else if(product === "carTools" && isInCart3 === false){
         quantity3++;
         document.getElementById("quantity3").innerHTML = quantity3;
     }
@@ -151,19 +185,19 @@ function addProduct(product) {
 }
 
 function removeProduct(product) {
-    if(product === "wheels"){
+    if(product === "wheels" && isInCart1 === false){
         if (quantity1 > 0) {
             quantity1--;
             document.getElementById("quantity1").innerHTML = quantity1;
         }
     }
-    else if(product === "cleaningKit"){
+    else if(product === "cleaningKit" && isInCart2 === false){
         if (quantity2 > 0) {
             quantity2--;
             document.getElementById("quantity2").innerHTML = quantity2;
         }
     }
-    else if(product === "carTools"){
+    else if(product === "carTools" && isInCart3 === false){
         if (quantity3 > 0) {
             quantity3--;
             document.getElementById("quantity3").innerHTML = quantity3;
